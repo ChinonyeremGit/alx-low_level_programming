6fba24a2279507014a1722679206ca50b326e69c
Peter Eziagor <petereziagor2000@gmail.com>
2022-09-09 20:17:18 -0700

    initial commit

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,14 +7,14 @@
 
 int main(void)
 {
-        char alpha = 'a';
-	
-        while (alpha <= 'z')
-        {
-                putchar(alpha);
-                alpha++;
-        }
+	char alpha = 'a';
+
+	while (alpha <= 'z')
+	{
+		putchar(alpha);
+		alpha++;
+	}
 	putchar('\n');
-	
-       	return (0);
+
+	return (0);
 }

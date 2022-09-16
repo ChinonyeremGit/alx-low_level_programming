15e174a838904d841e2f7c1d861c77d3e4fa8bd7
Peter Eziagor <petereziagor2000@gmail.com>
2022-09-15 20:08:43 -0700

    initail commit

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+*/
+
+/* betty style doc for function main goes there */
+void positive_or_negative(int i)
+{
+
+	/* your code goes there */
+	if (i > 0)
+	{
+		printf("%i is positive\n", i);
+	}
+	else if (i == 0)
+	{
+		printf("%i is zero\n", i);
+	}
+	else if (i < 0)
+	{
+		printf("%i is negative\n", i);
+	}
+	
+}

ec562086be84c9654089e032374218093233ae6f
Peter Eziagor <petereziagor2000@gmail.com>
2022-09-28 13:06:03 -0700

    5-sqrt_recursions.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,67 +1,33 @@
 #include "main.h"
-/**
- * root - function to calcualte root of number
- * @low: low number
- * @high: higheer number
- * @n: number to look for the square root.
- * Return: root of number
- */
-int root(int low, int high, int n);
 
 /**
- * _sqrt_recursion - function that returns the square of a num
- * @n: number to find the square root
- * Return: square root of n.
+ * find_root - find square root of n, starting from the smallest possible, 0
+ * @n: n
+ * @root: test this root
+ * Return: natural square root, or -1 if not natural root
  */
-int _sqrt_recursion(int n)
+
+int find_root(int n, int root)
 {
-	if (n < 0)
-	{
+	if (root * root > n)
 		return (-1);
-	}
-	else if (n <= 1)
-	{
-		return (n);
-	}
-	else
-	{
-		int low, high;
 
-		low = 0;
-		high = n;
-		return (root(low, high, n));
-	}
+	if (root * root == n)
+		return (root);
+
+	return (find_root(n, root + 1));
 }
 
 /**
- * root - function to calcualte root of number
- * @low: low number
- * @high: higheer number
- * @n: number to look for the square root.
- * Return: root of number
+ * _sqrt_recursion - find natural square root of n
+ * @n: n
+ * Return: natural square root, or -1 if not natural root
  */
-int root(int low, int high, int n)
-{
-	int mid;
 
-	mid = (low + high) / 2;
-	if (low == high && mid * mid != n)
-	{
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
 		return (-1);
-	}
-	else
-	{
-		if (mid * mid == n)
-		{
-			return (mid);
-		}
-		else if (mid * mid > n)
-		{
-			return (root(low, mid - 1, n));
-		}
-		else
-		{
-			return (root(mid + 1, high, n));
-		}
-	}
+
+	return (find_root(n, 0));
 }

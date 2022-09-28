9f477e0248634f64daa540634185e637a8420b3b
Peter Eziagor <petereziagor2000@gmail.com>
2022-09-28 12:13:51 -0700

    nicely implemented

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,67 @@
+#include "main.h"
+/**
+ * root - function to calcualte root of number
+ * @low: low number
+ * @high: higheer number
+ * @n: number to look for the square root.
+ * Return: root of number
+ */
+int root(int low, int high, int n);
+
+/**
+ * _sqrt_recursion - function that returns the square of a num
+ * @n: number to find the square root
+ * Return: square root of n.
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n <= 1)
+	{
+		return (n);
+	}
+	else
+	{
+		int low, high;
+
+		low = 0;
+		high = n;
+		return (root(low, high, n));
+	}
+}
+
+/**
+ * root - function to calcualte root of number
+ * @low: low number
+ * @high: higheer number
+ * @n: number to look for the square root.
+ * Return: root of number
+ */
+int root(int low, int high, int n)
+{
+	int mid;
+
+	mid = (low + high) / 2;
+	if (low == high && mid * mid != n)
+	{
+		return (-1);
+	}
+	else
+	{
+		if (mid * mid == n)
+		{
+			return (mid);
+		}
+		else if (mid * mid > n)
+		{
+			return (root(low, mid - 1, n));
+		}
+		else
+		{
+			return (root(mid + 1, high, n));
+		}
+	}
+}

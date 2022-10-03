13f5a343d4b9cbe3fdd754a44d7aa1ff716deb10
Peter Eziagor <petereziagor2000@gmail.com>
2022-10-03 16:06:05 -0700

    initial commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * @argc: argurment count
+ * @argv: argument vector
+ * Return: Return always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
+

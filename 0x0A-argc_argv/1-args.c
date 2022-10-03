c3c33c4c24a56489329d8426bc2e54f82d1e1af3
Peter Eziagor <petereziagor2000@gmail.com>
2022-10-03 16:31:31 -0700

    initial commit

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - mainn function
+ * @argc: argument count
+ * @argv: arguement vector
+ * Return: Always 0i.
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
+

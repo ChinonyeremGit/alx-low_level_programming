a10c66f86d2a1bc1e63e3feb262db6990bd7a6ee
Peter Eziagor <petereziagor2000@gmail.com>
2022-10-05 00:29:44 -0700

    initial commit

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,6 +17,8 @@ char *create_array(unsigned int size, char c)
 		return (NULL);
 	arr = malloc(size * (sizeof(c)));
 
+	if (arr == NULL)
+		return (NULL);
 	for (i = 0; i < size; i++)
 	{
 		arr[i] = c;

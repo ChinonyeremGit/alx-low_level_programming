33f8b23c7f5d297f815b00cb479c21154e33b443
Peter Eziagor <petereziagor2000@gmail.com>
2022-10-12 16:00:29 -0700

    initial commit

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,24 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - function to search an index
+ * @array: array to search from
+ * @size: size of the array
+ * @cmp: pointer to function to compare values
+ * Return: index to the first element that matches.
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+		return (-1);
+	if (array == NULL || cmp == NULL)
+		return (0);
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(*(array + i)))
+			return (i);
+	}
+	return (-1);
+}

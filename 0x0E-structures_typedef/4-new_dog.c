75881533d19fb25959633685a4db116aad657b76
Peter Eziagor <petereziagor2000@gmail.com>
2022-10-11 17:08:43 -0700

    created function to create an instance of a struct

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * new_dog - function to craete a new dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ * Return: instance of new dog
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *yudi;
+	yudi = malloc(sizeof(dog_t));
+	if (yudi == NULL)
+		return (NULL);
+	yudi->name = name;
+	yudi->age = age;
+	yudi->owner = owner;
+	return (yudi);
+}

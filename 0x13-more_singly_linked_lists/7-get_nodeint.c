c38b3d55e26d058635ccd063c6f5a16263b4d1b9
Peter Eziagor <petereziagor2000@gmail.com>
2022-10-25 20:47:25 -0700

    function to get address at a given position

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - function to get the address at a given position
+ * @head: pointer to first node
+ * @index: index to return its address
+ * Return: Address of nth node.
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *temp;
+	unsigned int len = 0;
+
+	temp = head;
+	if (temp == NULL)
+		return (NULL);
+	if (index == 0)
+		return (temp);
+	while (len < index - 1)
+	{
+		temp = temp->next;
+		len++;
+	}
+	return (temp->next);
+}

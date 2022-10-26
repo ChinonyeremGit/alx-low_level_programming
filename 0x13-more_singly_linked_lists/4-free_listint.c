b87cfd915ca9df3445ef652cb968323273c40d1a
Peter Eziagor <petereziagor2000@gmail.com>
2022-10-25 19:52:51 -0700

    free list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * free_listint - function to free list
+ * @head: pointer to first node in list
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head;
+		head = temp->next;
+		free(temp);
+	}
+}

5912e1ec3565d94a84dfc6cb596be9cc6479149d
Peter Eziagor <petereziagor2000@gmail.com>
2022-10-27 23:55:36 -0700

    reimplemented

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,51 +1,28 @@
 #include "main.h"
 
 /**
- * _stoi - function to convert string to unsigned number
- * @b: string
- * Return: unsigned number of string
- */
-unsigned int _stoi(const char *b)
-{
-	unsigned int num = 0;
-	int i = 0;
-
-	while (b[i] != 0)
-	{
-		num = num * 10 + b[i] - '0';
-		i++;
-	}
-	return (num);
-}
-
-/**
  * binary_to_uint - function to convert binary to uint
  * @b: string of 0 and 1 chars
  * Return: decimal on success or 0 if b is NULL or not 0 and 1
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int num;
-	unsigned int weight = 1;
 	unsigned int decimal = 0;
-	unsigned int rem;
-	int i = 0;
+	int i = 0, len = 0;
 
 	if (b == NULL)
 		return (0);
+	while (b[len] != 0)
+		len++;
+	len--;
 	while (b[i] != 0)
 	{
 		if (b[i] < 48 || b[i] > 49)
 			return (0);
+		if (b[i] == '1')
+			decimal = decimal + (1 * (1 << len));
 		i++;
-	}
-	num = _stoi(b);
-	while (num != 0)
-	{
-		rem = num % 10;
-		decimal = decimal + rem * weight;
-		weight = weight * 2;
-		num = num / 10;
+		len--;
 	}
 	return (decimal);
 }
